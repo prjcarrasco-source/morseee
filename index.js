985c89c775e88218ae8a815d8d83aa639c202c6e
prjcarrasco-source <prjcarrasco@kasnet.com>
2025-11-17 11:51:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
+// Middleware para CORS - permite peticiones desde cualquier origen
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Manejar preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
+
 // Middleware para parsear JSON
 app.use(express.json());
 
